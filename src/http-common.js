c6bf529d99862ead3f7d06c9df87315586ed8816
xavioo <rucahoxavi@gmail.com>
2022-01-15 17:22:33 +0200

    add entry file index.js and folder structure

--- src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://leave-app-sys.herokuapp.com/api/v1/coinmarket',
-  headers: {
-    'Content-type': 'application/json',
-  },
-});
