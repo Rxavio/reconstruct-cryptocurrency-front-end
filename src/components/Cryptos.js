ae3ee8a2d90e0d5c363175556290913328f8f2bb
xavioo <rucahoxavi@gmail.com>
2022-01-15 17:57:13 +0200

    Add ant design select component to select currencies

--- src/components/Cryptos.js
+++ src/components/Cryptos.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import {
+  Select,
   Typography
 } from "antd";
+
+const { Option } = Select;
 const { Title } = Typography;
+
 const Cryptocurrencies = () => {
+  
     return (
         <div>
 
@@ -21,11 +26,15 @@ const Cryptocurrencies = () => {
           Latest cryptocurrencies
         </Title>
 
-    
+        <Select
+          defaultValue="USD"
+          style={{ width: 120 }}
+        >
+          <Option value="1">USD</Option>
+          <Option value="0.89">EURO</Option>
+          <Option value="1015">RWF</Option>
+        </Select>
       </div>
-
-
-     
       </div>
     )
 }
