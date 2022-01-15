1233d2a44811cf0b4a92c07b41fc9d5d9aa6c4b1
xavioo <rucahoxavi@gmail.com>
2022-01-15 17:41:55 +0200

    setting routes index.js file

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import HomePage from '../layouts/HomePage';
+// import Cryptos from '../components/Cryptos';
+// import Portfolio from '../components/Portfolio';
+// import WatchList from '../components/watchList';
+// import Chart from '../components/Chart';
+
+const index = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path='/' element={<HomePage />}>
+          <Route path='' element={<Cryptos />} />
+          <Route path='portfolio' element={<Portfolio />} /> 
+          <Route path='watchList' element={<WatchList />} /> 
+          <Route path='chart' element={<Chart />} /> 
+        </Route> */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default index;
